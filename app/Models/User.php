726a59cf87ca8be1c291780ed99183b20ba55a60
Daniel Forson <danielkojoforson27@gmail.com>
2026-01-08 16:58:22 +0000

    feat: implement Chirp model, migration, and update ChirpController and home view to display chirps with user information

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,23 @@
         Welcome
     </x-slot:title>
      <div class="max-w-2xl mx-auto flex flex-col gap-4 mt-8">
-         @foreach ($chirps as $chirp )
-            <div class="card bg-base-100  shadow ">
+       @forelse ($chirps as $chirp)
+        <div class="card bg-base-100  shadow ">
                 <div class="card-body">
                     <div>
                        <div class="font-semibold">
-                        {{ $chirp['author'] }}
+                        {{ $chirp->user ? $chirp->user->name:'Anonymous' }}
                        </div>
-                       <div class="mt-1">{{ $chirp['message'] }}</div>
-                       <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                       <div class="mt-1">{{ $chirp->message }}</div>
+                       <div class="text-sm text-gray-500 mt-2">{{ $chirp->created_at->diffForHumans() }}</div>
                     </div>
                 </div>
             </div>
-             @endforeach
+
+       @empty
+
+       <p class="text-center text-gray-500">No chirps available.</p>
+
+       @endforelse
         </div>
 </x-layout>
